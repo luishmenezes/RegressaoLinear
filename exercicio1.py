0ec86819a4f2a28da724705e8ad0ef991a38b938
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2025-09-29 21:24:48 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+
+data = fetch_california_housing(as_frame=True)
+X = data.data
+y = data.target
+
+
+
+X_subset = X[['MedInc', 'HouseAge', 'AveRooms']]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_subset, y, test_size=0.2, random_state=0
+)
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+r2 = r2_score(y_test, y_pred)
+
+print("\n=== Exercício 1 — Baseline ===")
+print("MAE:", mae)
+print("RMSE:", rmse)
+print("R²:", r2)
+
